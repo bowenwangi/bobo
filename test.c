fb94fbea989f0eb3f737c8bcd1d401b72457f9df
bowenwangi <wwb66yx@163.com>
2019-08-12 16:07:26 +0800

    modified test.c

---
+++ test.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author:  波波 
+	> pleace: 上海  
+	> Created Time: Mon 12 Aug 2019 04:03:06 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc,const char *argv[])
+{        
+
+printf("hello world");
+return 0;
+
+}
